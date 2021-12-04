67595e677904095286c753271b7cd6298b6dd9f9
GaussYuan191 <1915059302@qq.com>
2021-12-05 00:04:31 +0800

    新增 搜索框动画

--- js/nav-search.js
+++ js/nav-search.js
@@ -1,48 +1,62 @@
 let searchBtn = document.getElementById("search-icon")
 let searchInput = document.getElementById("searchform-input")
-let searchformSrc = document.getElementById("searchform-src")
-let closeBtn = document.getElementById("search-close");
-let searchContent = document.getElementById("searchview");
-
-searchformSrc.value = ""
-searchInput.value = ""
+let closeBtn = document.getElementById("search-close")
+let searchContent = document.getElementById("searchview")
+let linkSearch = document.getElementById("link-search")
+let navContent = document.getElementsByClassName("nav-first")[0]
 searchInput.oninput = function() {
-    if (searchInput.value != "") searchInput.value = ""
-    searchInput.type = "hidden"
-    searchformSrc.type = "text"
-    console.log("ajshd")
-    searchformSrc.onfocus()
+    
     
 }
-
-searchformSrc.oninput = function() {
-    searchInput.value = ""
-    console.log("ajshdaa")
+inputInit = function() {
+    searchInput.value =  ""
+    console.log('初始化输入框')
 }
-
-
+navAnimation = function() {
+    let navList = Array.from(navContent.childNodes)
+    let newList = navList.filter((item, index) => {
+        console.log(item, index)
+        return index % 2 != 0
+      })
+      navList.forEach((item) => {
+        item.className = "nav-animation"
+      })
+}
+linkSearch.onclick = function() {
+    searchContent.style.display = "block"
+    inputInit()
+    navAnimation()
+    navContent.style.display = "none"
+    searchInput.focus()
+    console.log(searchInput.onfocus)
+}
 closeBtn.onclick = function() {
     searchContent.style.display = "none"
+    navContent.style.display = "flex"
 }
 
 document.onclick = function(e) {
-    ev = e || window.event; 
-    var mousePos = mouseCoords(ev); 
-    if ((mousePos.x > 1300 || mousePos.x < 610) && (mousePos.y > 44)) {
-        // searchContent.style.display = "none"
-        searchformSrc.value = ""
+    ev = e || window.event
+    var mousePos = mouseCoords(ev)
+    let flag = searchContent.style.display
+     console.log(flag)
+    if (flag == "block" && (mousePos.x > 1300 || mousePos.x < 610) && (mousePos.y > 44)) {
+        searchContent.style.display = "none"
+        navContent.style.display = "flex"
+        inputInit()
     }
+    console.log(mousePos.x, mousePos.y)
     
 }
 function mouseCoords(ev) 
  { 
     //鼠标移动的位置
      if(ev.pageX || ev.pageY){ 
-         return {x:ev.pageX, y:ev.pageY}; 
+         return {x:ev.pageX, y:ev.pageY}
      } 
     return{ 
          x:ev.clientX + document.body.scrollLeft - document.body.clientLeft, 
          y:ev.clientY + document.body.scrollTop - document.body.clientTop 
-    }; 
+    }
  } 
 
