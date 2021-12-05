715036437e93aa88834a92e530f6869f9b58b7ee
GaussYuan191 <1915059302@qq.com>
2021-12-05 22:37:19 +0800

    完善 搜索区域逻辑

--- js/nav-search.js
+++ js/nav-search.js
@@ -4,56 +4,82 @@ let closeBtn = document.getElementById("search-close")
 let searchContent = document.getElementById("searchview")
 let linkSearch = document.getElementById("link-search")
 let navContent = document.getElementsByClassName("nav-first")[0]
+let resultContent = document.getElementsByClassName("searchresults-list")[0]
+let resultList = Array.from(resultContent.childNodes)
+let searchresultContent = document.getElementById("searchresults")
+let itemList =  resultList.filter((item, index) => {
+    return index % 2 != 0
+})
+let searchResults = document.getElementById("searchresults")
 searchInput.oninput = function() {
-    
+    itemList.forEach((item) => {
+        if (searchInput.value == "") {
+            item.childNodes[0].innerText = "Apple Store 商店"
+        } else {
+            item.childNodes[0].innerText = searchInput.value
+        }
+        
+    })
     
 }
+//初始化搜索区域
 inputInit = function() {
     searchInput.value =  ""
-    console.log('初始化输入框')
+    itemList.forEach((item) => {
+        item.childNodes[0].innerHTML = "Apple Store 商店"
+    })  
 }
+//给导航栏添加动画
 navAnimation = function() {
     let navList = Array.from(navContent.childNodes)
     let newList = navList.filter((item, index) => {
-        console.log(item, index)
         return index % 2 != 0
       })
-      navList.forEach((item) => {
+    newList.forEach((item) => {
         item.className = "nav-animation"
-      })
+    })
 }
+
+//点击搜索图标出现搜索框隐藏导航栏
 linkSearch.onclick = function() {
+    console.log(navContent.style.display,searchContent.style.display ,searchResults.style.display)
+    navContent.style.display = "none"
     searchContent.style.display = "block"
+    searchresultContent.style.display = "block"
+    searchresultContent.style.position = "fixed"
     inputInit()
     navAnimation()
-    navContent.style.display = "none"
     searchInput.focus()
-    console.log(searchInput.onfocus)
+    console.log(navContent.style.display,searchContent.style.display ,searchResults.style.display)
+
+    
 }
+
+//点击关闭图标出现导航栏隐藏搜索框
 closeBtn.onclick = function() {
     searchContent.style.display = "none"
     navContent.style.display = "flex"
+    searchresultContent.style.display = "none"
 }
-
+//当搜索框出现时，点击搜索框以外的区域，搜索区域隐藏
 document.onclick = function(e) {
     ev = e || window.event
     var mousePos = mouseCoords(ev)
     let flag = searchContent.style.display
-     console.log(flag)
-    if (flag == "block" && (mousePos.x > 1300 || mousePos.x < 610) && (mousePos.y > 44)) {
-        searchContent.style.display = "none"
-        navContent.style.display = "flex"
+    if (flag == "block" && (mousePos.x > 1300 || mousePos.x < 610) && (mousePos.y > 84)) {
+        closeBtn.onclick()
         inputInit()
     }
     console.log(mousePos.x, mousePos.y)
     
 }
+//监测鼠标点击的位置
 function mouseCoords(ev) 
  { 
     //鼠标移动的位置
-     if(ev.pageX || ev.pageY){ 
-         return {x:ev.pageX, y:ev.pageY}
-     } 
+    if(ev.pageX || ev.pageY){ 
+        return {x:ev.pageX, y:ev.pageY}
+    } 
     return{ 
          x:ev.clientX + document.body.scrollLeft - document.body.clientLeft, 
          y:ev.clientY + document.body.scrollTop - document.body.clientTop 
