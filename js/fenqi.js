b0890c646812f179487c167555c4472358402c52
GaussYuan191 <1915059302@qq.com>
2021-12-05 22:35:55 +0800

    新增 分期js

---
+++ js/fenqi.js
@@ -0,0 +1,8 @@
+function openDialog(){
+    document.getElementById('light').style.display='block'
+    document.getElementById('fade').style.display='block'
+}
+function closeDialog(){
+    document.getElementById('light').style.display='none'
+    document.getElementById('fade').style.display='none'
+}
