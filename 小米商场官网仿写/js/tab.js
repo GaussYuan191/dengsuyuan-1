dea3ed0075fce4d9ccc9b34a6f327e0e89de001b
GaussYuan191 <1915059302@qq.com>
2021-12-12 20:59:42 +0800

    新增 tabjs

---
+++ 小米商场官网仿写/js/tab.js
@@ -0,0 +1,21 @@
+function tabS(obj) {
+    var tabWrap = document.querySelector(obj);
+    var tabList = tabWrap.querySelectorAll(".tab-list li");
+    var tabUl = tabWrap.querySelectorAll(".appliances-list");
+
+    tabList[0].classList.add("tab-active");
+    for (var i = 0; i < tabList.length; i++) {
+        tabList[i].num = i;
+        tabList[i].onmouseover = function() {
+            for (var i = 0; i < tabList.length; i++) {
+                tabList[i].className = "";
+            }
+            tabList[this.num].classList.add("tab-active");
+            for (var j = 0; j < tabUl.length; j++) {
+                tabUl[j].classList.add("hide");
+            }
+            tabUl[this.num].classList.remove("hide");
+        }
+    }
+}
+tabS(".home-appliances-box")
