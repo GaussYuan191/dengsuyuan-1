676b3b63a6a45c53d2aa343722d1e5574d101faf
dengsuyuan <dengsuyuan@ysstech.com>
2022-01-02 21:43:26 +0800

    修改

--- js/fenqi.js
+++
@@ -1,8 +0,0 @@
-function openDialog(){
-    document.getElementById('light').style.display='block'
-    document.getElementById('fade').style.display='block'
-}
-function closeDialog(){
-    document.getElementById('light').style.display='none'
-    document.getElementById('fade').style.display='none'
-}
